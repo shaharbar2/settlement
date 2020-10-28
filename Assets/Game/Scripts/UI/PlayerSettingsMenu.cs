84ab49b59eace049e6fc664cd3ef2e2f32e592fe
Kirill Nepomnyaschiy <nka1024@gmail.com>
2020-10-29 04:39:14 +0700

    version bump

--- Assets/Game/Scripts/UI/PlayerSettingsMenu.cs
+++ Assets/Game/Scripts/UI/PlayerSettingsMenu.cs
@@ -9,12 +9,12 @@ public class PlayerSettingsMenu : MonoBehaviour {
     [SerializeField] public Dropdown dropdownCamera;
     [SerializeField] public Dropdown dropdownBuild;
     [SerializeField] public Button closeButton;
+    [SerializeField] public TMP_Text versionText;
 
     public delegate void PlayerSettingsMenuEvent();
     public event PlayerSettingsMenuEvent onClose;
 
     void Start() {
-        
         dropdownMovement.onValueChanged.AddListener(onDropdownMovementValueChanged);
         dropdownMovement.value = (int)Constants.instance.PLAYER_CONTROL_STYLE;
         dropdownCamera.onValueChanged.AddListener(onDropdownCameraValueChanged);
@@ -22,6 +22,7 @@ public class PlayerSettingsMenu : MonoBehaviour {
         dropdownBuild.onValueChanged.AddListener(onDropdownBuildValueChanged);
         dropdownBuild.value = (int)Constants.instance.BUILDING_STYLE;
         closeButton.onClick.AddListener(onCloseButtonClick);
+        versionText.text = "build " + Application.version;
     }
 
     // Update is called once per frame
