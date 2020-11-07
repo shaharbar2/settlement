5e292c03015ddaf8fd0ce740e1d59dacaaf2304b
Kirill Nepomnyaschiy <nka1024@gmail.com>
2020-11-08 00:57:07 +0700

    handle default building on scene load

---
+++ Assets/Game/Scripts/OnSceneLoad.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnSceneLoad : MonoBehaviour {
+
+    private TilemapController tilemapController;
+    private BuildingPrefab[] defaultBuildings;
+
+    void Awake() {
+        tilemapController = FindObjectOfType<TilemapController>();
+
+        manageDefaultBuildingsAwake();
+    }
+
+    void Start() {
+        manageDefaultBuildingsStart();
+    }
+
+    /// Private -- 
+    
+    private void manageDefaultBuildingsAwake() {
+        defaultBuildings = FindObjectsOfType<BuildingPrefab>();
+        foreach(var building in defaultBuildings) {
+            building.buildOnStart = true;
+            building.instantBuild = true;
+            building.transform.position = tilemapController.snap(building.transform.position);
+        }
+    }
+
+    private void manageDefaultBuildingsStart() {
+        foreach(var building in defaultBuildings) {
+            BuildingData buildingData = BuildingConfiguration.instance.buildingDataFor(building.type);
+            tilemapController.markUnwalkable(building.transform.position, buildingData.areaType);
+        }
+    }
+}
